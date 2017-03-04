5ae02cd8f569432bb8e60ece4d35a0b32af1e852
Deepak Malik <deepak.malik174266@gmail.com>
2017-03-04 08:32:59 -0800

    Problem 15 - Math

--- src/com/deepak/java/problems/Math/Problem_15.java
+++ src/com/deepak/java/problems/Math/Problem_15.java
@@ -1,8 +1,16 @@
+/**
+ * JavaProblems
+ * Problem_15.java
+ */
 package com.deepak.java.problems.Math;
 
 /**
+ * <br> Problem Statement :
+ * 
  * Given a set of parenthesis of different types 
- * find if they are valid
+ * find if they are valid combination
+ * 
+ * </br>
  * 
  * @author Deepak
  */
@@ -11,13 +19,18 @@ public class Problem_15 {
 	/**
 	 * Method to check if input has valid parenthesis
 	 * 
+	 * Time Complexity : O(n) => Where n is number of parenthesis in input
+	 * Space Complexity : O(1)
+	 * 
 	 * @param input
-	 * @return
+	 * @return {@link boolean}
 	 */
 	public static boolean isValidParenthesis(String input) {
-		int braceCount = 0; // {
-		int parenthesisCount = 0; // (
-		int squareBracketCount = 0; // [
+		int braceCount = 0; /* { */
+		int parenthesisCount = 0; /* ( */
+		int squareBracketCount = 0; /* [ */
+		/* Convert to character array and check each one of them.
+		 * Maintain count for each pair */
 		char[] charArray = input.toCharArray();
 		for (char c : charArray) {
 			switch (c) {
@@ -44,6 +57,8 @@ public class Problem_15 {
 				break;
 			}
 		}
+		/* Valid pairs will exist if and only if 
+		 * input has same number of opening and closing braces. */
 		return braceCount == 0 && parenthesisCount == 0 && squareBracketCount == 0;
 	}
 
