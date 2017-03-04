b460e140eb610e58bf42d703e2a70f54ec9332ab
Deepak Malik <deepak.malik174266@gmail.com>
2017-03-03 16:41:10 -0800

    Problem 11 - Math

--- src/com/deepak/java/problems/Math/Problem_11.java
+++ src/com/deepak/java/problems/Math/Problem_11.java
@@ -1,7 +1,15 @@
+/**
+ * JavaProblems
+ * Problem_11.java
+ */
 package com.deepak.java.problems.Math;
 
 /**
+ * <br> Problem Statement :
+ * 
  * Find if a given number is power of 2
+ * 
+ * </br>
  * 
  * @author Deepak
  */
@@ -9,6 +17,9 @@ public class Problem_11 {
 	
 	/**
 	 * Method to check if a number is power of 2
+	 * 
+	 * Time Complexity = O(1)
+	 * Space Complexity = O(1) 
 	 * 
 	 * @param n
 	 * @return {@link boolean}
