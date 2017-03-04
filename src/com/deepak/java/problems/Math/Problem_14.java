6b9d3303fa28f92918239c90c093cba6c1615e80
Deepak Malik <deepak.malik174266@gmail.com>
2017-03-04 08:28:16 -0800

    Problem 14 - Math

--- src/com/deepak/java/problems/Math/Problem_14.java
+++ src/com/deepak/java/problems/Math/Problem_14.java
@@ -1,7 +1,15 @@
+/**
+ * JavaProblems
+ * Problem_14.java
+ */
 package com.deepak.java.problems.Math;
 
 /**
+ * <br> Problem Statement
+ * 
  * Implement FizzBuzz till a given number n
+ * 
+ * </br>
  * 
  * @author Deepak
  */
@@ -10,10 +18,16 @@ public class Problem_14 {
 	/**
 	 * Method to print fizz Buzz
 	 * 
+	 * Time Complexity : O(n)
+	 * Space Complexity : O(1)
+	 * 
 	 * @param n
 	 */
 	public static void fizzBuzz(int n) {
+		/* Fizz buzz cannot happen on negative numbers */
 		if (n < 0) return;
+		/* Check for each number, if divisible by 3 or 5 or both.
+		 * Print accordingly */
 		for (int i = 1; i <= n; i++) {
 			if (i % 3 == 0 && i % 5 == 0) {
 				System.out.println("FizzBuzz");
