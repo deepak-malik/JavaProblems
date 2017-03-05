d4ee937f2aaf5b648421937b587f31261c8b0394
Deepak Malik <deepak.malik174266@gmail.com>
2017-03-05 08:06:32 -0800

    Problem 22 - Math

--- src/com/deepak/java/problems/Math/Problem_22.java
+++ src/com/deepak/java/problems/Math/Problem_22.java
@@ -17,6 +17,7 @@ public class Problem_22 {
 
 	/**
 	 * Method to check if a given string is a valid number
+	 * This is brute force approach which does not handle lot of cases, for ex. doubles
 	 * 
 	 * Time Complexity : O(n) => where n is the number of characters in string
 	 * Space Complexity : O(n)
@@ -24,7 +25,7 @@ public class Problem_22 {
 	 * @param s
 	 * @return {@link boolean}
 	 */
-	public static boolean isValidNumber(String s) {
+	public static boolean isValidNumber_BF(String s) {
 		if (s == null || s.length() == 0) {
 			return false;
 		}
@@ -40,9 +41,78 @@ public class Problem_22 {
 		return true;
 	}
 
+	/* We will be using state machine in this approach */
+	public enum STATE {START, INTEGER, DECIMAL, UNKNOWN}
+
+	/**
+	 * Method to check if a number is valid
+	 * 
+	 * @param s
+	 * @return {@link boolean}
+	 */
+	public static boolean isValidNumber_Optimized(String s) {
+		/* Return false for empty string */
+		if (s.isEmpty()) return false;
+		/* Ignore + or - */
+		int i = 0;
+		if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+			i++;
+		}
+		/* Assume state as START and keep checking state for each character */
+		STATE currentState = STATE.START;
+		while (i < s.length()) {
+			currentState = getNextState(currentState, s.charAt(i));
+			/* If at any point we reach unknown state, return false */
+			if (currentState == STATE.UNKNOWN) {
+				return false;
+			}
+			i++;
+		}
+		return true;
+	}
+
+	/**
+	 * Method to check the next state of the character 
+	 * 
+	 * @param currentState
+	 * @param c
+	 * @return {@link STATE}
+	 */
+	private static STATE getNextState(STATE currentState, char c) {
+		/* If start, check for others, 
+		 * If Integer, check for decimal or valid integer 
+		 * If Decimal, check next character is number, if yes, stay at Decimal */
+		switch (currentState) {
+		case START:
+		case INTEGER :
+			if (c == '.') {
+				return STATE.DECIMAL;
+			} else if (c >= '0' && c <= '9') {
+				return STATE.INTEGER;
+			} else {
+				return STATE.UNKNOWN;
+			}
+		case DECIMAL :
+			if (c >= '0' && c <= '9') {
+				return STATE.DECIMAL;
+			} else {
+				return STATE.UNKNOWN;
+			}
+		default:
+			break;
+		}
+		return STATE.UNKNOWN;
+	}
+
 	public static void main(String[] args) {
-		System.out.println(isValidNumber("3"));
-		System.out.println(isValidNumber("-39073097"));
+		System.out.println(isValidNumber_BF("3"));
+		System.out.println(isValidNumber_BF("-39073097"));
+		System.out.println(isValidNumber_Optimized("3"));
+		System.out.println(isValidNumber_Optimized("-39073097"));
+		System.out.println(isValidNumber_Optimized("4.325"));
+		System.out.println(isValidNumber_Optimized("1.1.1"));
+		System.out.println(isValidNumber_Optimized("33."));
+		System.out.println(isValidNumber_Optimized("22.22."));
 	}
 
 }
